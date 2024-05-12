07ec4aa64e448a4251a48c56f08114dc4ba2e9e0
Murat YILDIZ <programingworks01@gmail.com>
2024-05-12 14:21:35 +0300

    Update

--- Presentation/Thema/src/app/pages/dashboard/DashboardWrapper.tsx
+++ Presentation/Thema/src/app/pages/dashboard/DashboardWrapper.tsx
@@ -2,27 +2,12 @@ import { FC } from 'react'
 import { useIntl } from 'react-intl'
 import { toAbsoluteUrl } from '../../../_metronic/helpers'
 import { PageTitle } from '../../../_metronic/layout/core'
-import {
-  ListsWidget2,
-  ListsWidget3,
-  ListsWidget4,
-  ListsWidget6,
-  TablesWidget5,
-  TablesWidget10,
-  MixedWidget8,
-  CardsWidget7,
-  CardsWidget17,
-  CardsWidget20,
-  ListsWidget26,
-  EngageWidget10,
-} from '../../../_metronic/partials/widgets'
-import { Content } from '../../../_metronic/layout/components/content'
 import { auto } from '@popperjs/core'
 
 const DashboardPage: FC = () => (
   <>
-    <div style={{ marginBottom: auto }}>
-      <div id="kt_carousel_3_carousel" className="carousel carousel-custom slide mx-auto" style={{ maxHeight: "300px" }} data-bs-ride="carousel" data-bs-interval="8000">
+    <div style={{ marginBottom: "80px",  }}>
+      <div id="kt_carousel_3_carousel" className="carousel carousel-custom slide mx-auto" style={{ maxHeight: "300px"  }} data-bs-ride="carousel" data-bs-interval="8000">
         <div className="d-flex align-items-center justify-content-between flex-wrap">
           <span className="fs-4 fw-bold pe-2">Title</span>
           <ol className="p-0 m-0 carousel-indicators carousel-indicators-bullet carousel-indicators-active-primary">
@@ -35,32 +20,68 @@ const DashboardPage: FC = () => (
           <div className="carousel-item active">
             <div className="bgi-no-repeat rounded mb-6" style={{ backgroundImage: `url(https://picsum.photos/1450/300)`, backgroundSize: "cover", height: "300px", width: auto, marginLeft: "auto", marginRight: "auto" }}></div>
           </div>
-
           <div className="carousel-item">
             <div className="bgi-no-repeat rounded mb-6" style={{ backgroundImage: `url(https://picsum.photos/1450/300)`, backgroundSize: "cover", height: "300px", width: auto, marginLeft: "auto", marginRight: "auto" }}></div>
           </div>
-
           <div className="carousel-item">
             <div className="bgi-no-repeat rounded mb-6" style={{ backgroundImage: `url(https://picsum.photos/1450/300)`, backgroundSize: "cover", height: "300px", width: auto, marginLeft: "auto", marginRight: "auto" }}></div>
           </div>
         </div>
       </div>
     </div>
-    <div className="mt-4">
-      <div className="card bg-light shadow-sm">
+    <div className="mt-4 d-flex justify-content-between">
+      <div className="card bg-light shadow-sm me-2" style={{ width: "calc(50% - 0.5rem)" }}>
         <div className="card-header">
           <h3 className="card-title">Title</h3>
           <div className="card-toolbar">
             <button type="button" className="btn btn-sm btn-light">
-              Action
+              About
             </button>
           </div>
         </div>
         <div className="card-body card-scroll h-200px">
-          Lorem Ipsum is simply dummy text...
+          "Space" is the term used to describe the vast expanse beyond Earth's atmosphere, encompassing all matter, energy, time, and space itself. It is believed to be the infinite extent in which stars, galaxies, planets, and other celestial bodies exist. According to the Big Bang theory, the universe began approximately 13.8 billion years ago, and since then, it has been expanding. Space is a fundamental concept studied in fields such as astronomy, physics, and cosmology.
         </div>
-        <div className="card-footer">
-          Footer
+      </div>
+      <div className="card bg-light shadow-sm" style={{ width: "calc(50% - 0.5rem)"}}>
+        <div className="card-header">
+          <h3 className="card-title">Title</h3>
+          <div className="card-toolbar">
+            <button type="button" className="btn btn-sm btn-light">
+              About
+            </button>
+          </div>
+        </div>
+        <div className="card-body card-scroll h-200px">
+          "Space" is the term used to describe the vast expanse beyond Earth's atmosphere, encompassing all matter, energy, time, and space itself. It is believed to be the infinite extent in which stars, galaxies, planets, and other celestial bodies exist. According to the Big Bang theory, the universe began approximately 13.8 billion years ago, and since then, it has been expanding. Space is a fundamental concept studied in fields such as astronomy, physics, and cosmology.
+        </div>
+      </div>
+    </div>
+    <div className="mt-2 d-flex justify-content-between">
+      <div className="card bg-light shadow-sm me-2" style={{ width: "calc(50% - 0.5rem)" }}>
+        <div className="card-header">
+          <h3 className="card-title">Title</h3>
+          <div className="card-toolbar">
+            <button type="button" className="btn btn-sm btn-light">
+              About
+            </button>
+          </div>
+        </div>
+        <div className="card-body card-scroll h-200px">
+          "Space" is the term used to describe the vast expanse beyond Earth's atmosphere, encompassing all matter, energy, time, and space itself. It is believed to be the infinite extent in which stars, galaxies, planets, and other celestial bodies exist. According to the Big Bang theory, the universe began approximately 13.8 billion years ago, and since then, it has been expanding. Space is a fundamental concept studied in fields such as astronomy, physics, and cosmology.
+        </div>
+      </div>
+      <div className="card bg-light shadow-sm" style={{ width: "calc(50% - 0.5rem)" }}>
+        <div className="card-header">
+          <h3 className="card-title">Title</h3>
+          <div className="card-toolbar">
+            <button type="button" className="btn btn-sm btn-light">
+              About
+            </button>
+          </div>
+        </div>
+        <div className="card-body card-scroll h-200px">
+          "Space" is the term used to describe the vast expanse beyond Earth's atmosphere, encompassing all matter, energy, time, and space itself. It is believed to be the infinite extent in which stars, galaxies, planets, and other celestial bodies exist. According to the Big Bang theory, the universe began approximately 13.8 billion years ago, and since then, it has been expanding. Space is a fundamental concept studied in fields such as astronomy, physics, and cosmology.
         </div>
       </div>
     </div>
